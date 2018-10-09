3fd7293f00513d412b770bfb0a3c53e4e7c5a92d
Rahul Verma <rahulnew140389@outlook.com>
2018-10-09 13:28:21 +0530

    changes set 4

--- routes/login.js
+++ routes/login.js
@@ -1,26 +1,24 @@
 var express = require("express");
 var router = express.Router();
-var db = require("../libs/db.js");
+var path = require("path");
 const auth = require("../libs/auth.js");
 
-/* Used to load the static folder and files */
-// router.use('/', express.static(__dirname + ''));
-
-/* To set the path of the static html file to be served. */
-var path = require("path");
-
-router.post("/login", auth.createToken, function(req, res){
-  console.log(req);
-  var user = db.collection("login");
-  user.find({ username: req.body.username }).toArray(function(err, docs) {
-    var userObject = docs[0];
-    if (userObject) {
-      res.json({
-        token: res.token
-      });
+var mongoClient = require("mongodb").MongoClient;
+var db;
+/* Connecting with Mongo DB */
+mongoClient.connect(
+  "mongodb://localhost:27017",
+  (err, client) => {
+    if (err) {
+      console.log("error occured while retriving");
+      return;
+    } else {
+      console.log("Connected to Mongo DB");
     }
-  });
-});
+
+    db = client.db("farmersowndb");
+  }
+);
 
 router.post("/admin-login", auth.createToken, (req, res) => {
   console.log(req.body);
